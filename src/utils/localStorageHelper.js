1b8e96490e74d4954f725c17274fc0d922fd7bb9
ishwar rimal <ishwarrimal@ishwars-MacBook-Pro.local>
2024-11-19 16:16:53 +0530

    fix added try catch for localstorage

--- src/utils/localStorageHelper.js
+++ src/utils/localStorageHelper.js
@@ -1,8 +1,18 @@
 export const saveScoreToLocalStorage = (score, key = 'userScore') => {
-    localStorage.setItem(key, JSON.stringify(score));
+    try{
+      localStorage.setItem(key, JSON.stringify(score));
+    }catch(e){
+      //Log the error
+      return null
+    }
   };
   
 export const fetchScoreFromLocalStorage = (key='userScore') => {
-  const score = localStorage.getItem(key);
-  return score ? JSON.parse(score) : null;
+  try{
+    const score = localStorage.getItem(key);
+    return score ? JSON.parse(score) : null;
+  }catch(e){
+    //Log the error
+    return null
+  }
 };
